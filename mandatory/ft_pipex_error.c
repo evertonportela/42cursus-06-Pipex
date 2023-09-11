f5072c07a7e63e9a3847eda7b18e5c8cba2bc523
evportel <evportel@student.42sp.org.br>
2023-09-10 23:04:34 -0300

    fix: norm file ft_pipex_error

--- mandatory/ft_pipex_error.c
+++ mandatory/ft_pipex_error.c
@@ -6,21 +6,19 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 19:04:19 by evportel          #+#    #+#             */
-/*   Updated: 2023/09/10 21:19:02 by evportel         ###   ########.fr       */
+/*   Updated: 2023/09/10 23:03:48 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
+void	ft_pipex_error(void)
 /**
  * Função para lidar com erros no programa Pipex.
  * Imprime uma mensagem de erro padrão no stderr
  * e encerra o programa com código de falha.
  */
-void ft_pipex_error(void)
 {
-	// Imprime uma mensagem de erro com um prefixo "Pipex Error"
 	perror("Pipex Error");
-	// Encerra o programa com código de saída indicando falha.
 	exit(EXIT_FAILURE);
 }
