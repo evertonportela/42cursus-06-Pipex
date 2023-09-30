a65e37ac23235e3ea5dad520476b3c98b34fa86b
evportel <evportel@student.42sp.org.br>
2023-09-30 15:41:29 -0300

    update: espaço na mensagem de erro

--- mandatory/ft_open_file.c
+++ mandatory/ft_open_file.c
@@ -6,7 +6,7 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 15:43:24 by evportel          #+#    #+#             */
-/*   Updated: 2023/09/28 22:28:47 by evportel         ###   ########.fr       */
+/*   Updated: 2023/09/30 15:41:04 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_open_file(char *file, int io_flag)
 		fd = open(file, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 	{
-		write(2, "Pipex Error: Failed to open file:", 34);
+		write(2, "Pipex Error: Failed to open file: ", 35);
 		write(2, file, ft_strlen(file));
 		exit(EXIT_FAILURE);
 	}
